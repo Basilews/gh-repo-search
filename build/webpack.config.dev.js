cfde34afd6a94701e33ba9f8bf1125e5a4cac91a
Basilews <dethroll@gmail.com>
2018-11-10 18:18:38 +0300

    add first markup

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      <label for="search">Search GH repos:</label>
+    </h1>
+    <input type="text" name="search" autofocus>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  h1
+    margin-bottom: 10px
+    font-size: 18px
+    font-weight: 600
+
+  input
+    padding-left: 5px
+    font-size: 16px
+    border: 1px solid black
+</style>
